bccf67e396033547de6a73b2612ac6ecdcd47172
s-a-b-r <po_fasty_grisha@mail.ru>
2023-09-30 23:55:44 +0400

    Закончил первую секцию под мобилки

---
+++ src/components/StatsComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex flex-col">
+        <span class="font-semibold text-xl">{{count}}</span>
+        <slot></slot>
+    </div>
+</template>
+
+<script setup>
+import {defineProps} from 'vue';
+
+const props = defineProps({
+    count: String,
+    title: String,
+})
+
+</script>
+
+<style scoped>
+
+</style>
