633ca6fb62d6daffaf928efc8c9057bdbdd101d0
s-a-b-r <po_fasty_grisha@mail.ru>
2023-10-02 00:19:20 +0400

    Начал вторую секцию на мобилках

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,12 +2,20 @@
     <div>
         <HeaderComponent></HeaderComponent>
         <FirstSectionComponent></FirstSectionComponent>
+        <section class="content_container">
+            <div class="mb-10">
+                <h2 class="second_title">Trending Collection</h2>
+                <span>Checkout our weekly updated trending collection.</span>
+            </div>
+            <CollectionComponent :collectionName="'animals'"></CollectionComponent>
+        </section>
     </div>
 </template>
 
 <script setup>
 import HeaderComponent from '@/components/HeaderComponent.vue';
 import FirstSectionComponent from '@/components/FirstSection/FirstSectionComponent.vue';
+import CollectionComponent from '@/components/TrendingCollection/CollectionComponent.vue';
 </script>
 
 <style scoped>
