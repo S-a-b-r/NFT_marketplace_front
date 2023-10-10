af53b392e854b128f311a9aa258d21aaeb411e90
s-a-b-r <po_fasty_grisha@mail.ru>
2023-10-10 22:43:16 +0400

    Доделал категории под десктопы + поменял анимацию

--- src/components/CategoryItem.vue
+++ src/components/CategoryItem.vue
@@ -8,24 +8,22 @@ const props = defineProps({
 
 <template>
     <router-link to="/categories" class="category_item_wrapper rounded bg-gray overflow-hidden">
-        <div class=" w-full h-[142px] bg-cover bg-center" 
+        <div class=" w-full h-[142px] lg:h-[240px] bg-cover bg-center" 
             :style="`background-image:url('src/assets/images/categories/${props.category.bgUrl}.png')`">
             <div class="bg_blur w-full h-full flex align-middle justify-center">
                 <img height="80" width="80" class="category_icon" :src="`src/assets/images/categories/icons/${props.category.iconUrl}.svg`" />
             </div>
         </div>
-        <div class="p-5">
+        <div class="p-5 font-semibold lg:text-xl">
             {{ props.category.title }}
         </div>
     </router-link>
 </template>
 
 <style>
-.category_item_wrapper:hover .bg_blur{
-    backdrop-filter: blur(0px);
-}
 .category_item_wrapper:hover .category_icon{
-    display: none;;
+    transition: 0.2s;
+    width: 130px;
 }
 .bg_blur{
     backdrop-filter: blur(5px);
