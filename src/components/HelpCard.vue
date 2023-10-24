c707b0b17f2b6343411ae59218c9e15cae831dff
s-a-b-r <po_fasty_grisha@mail.ru>
2023-10-24 23:33:47 +0400

    Доделал блок How It Works

--- src/components/HelpCard.vue
+++ src/components/HelpCard.vue
@@ -8,13 +8,14 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="w-full bg-gray flex justify-between">
-        <div>
-            <img :src="props.helpCardInfo.imgUrl" />
+    <div class="w-full bg-gray flex justify-between rounded p-5
+            md:flex-col">
+        <div class="w-5/12 self-center md:w-full md:mb-5">
+            <img class="w-[100px] h-[100px] md:w-[160px] md:h-[160px] md:m-auto lg:h-[250px] lg:w-[250px]" :src="props.helpCardInfo.imgUrl" />
         </div>
-        <div>
-            <h3 class="font-semibold">{{ props.helpCardInfo.title }}</h3>
-            <div class="text-sm">{{ props.helpCardInfo.description }}</div>
+        <div class="w-7/12 md:w-full md:text-center">
+            <h3 class="font-semibold mb-2 lg:text-xl">{{ props.helpCardInfo.title }}</h3>
+            <div class="text-sm leading-5 lg:text-base">{{ props.helpCardInfo.description }}</div>
         </div>
     </div>
 </template>
--- src/components/sections/HowItWorksSection.vue
+++ src/components/sections/HowItWorksSection.vue
@@ -1,22 +1,33 @@
 <script setup>
 import HelpCard from "@/components/HelpCard.vue";
 
-const imagesPath = 'src/assets/images/info_card_icons';
+const imagesPath = 'src/assets/images/info_card_icons/';
 
 const helpCards = [
     {
-        imgUrl: imagesPath + 'HelpIcon1.png',
+        imgUrl: imagesPath + 'HelpIcon2.png',
         title: 'Setup Your wallet',
         description: 'Set up your wallet of choice. Connect it to the NFT market by clicking the wallet icon in the top right corner.'
-    }
+    },
+    {
+        imgUrl: imagesPath + 'HelpIcon1.png',
+        title: 'Create Collection',
+        description: 'Upload your work and setup your collection. Add a description, social links and floor price.'
+    },
+    {
+        imgUrl: imagesPath + 'HelpIcon3.png',
+        title: 'Start Earning',
+        description: 'Choose between auctions and fixed-price listings. Start earning by selling your NFTs or trading others.'
+    },
 ]
 </script>
 
 <template>
     <section class="content_container">
-        <h2 class="second_title">How It Works</h2>
-        <div>Find out how to get started</div>
-        
-        <HelpCard v-for="helpCard in helpCards" :helpCardInfo="helpCard" :key="helpCard.title" />
+        <h2 class="second_title mb-2">How It Works</h2>
+        <div class="mb-10 lg:mb-12">Find out how to get started</div>
+        <div class="flex gap-5 flex-col md:flex-row">
+            <HelpCard v-for="helpCard in helpCards" :helpCardInfo="helpCard" :key="helpCard.title" />
+        </div>
     </section>
 </template>
