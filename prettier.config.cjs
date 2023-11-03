fcbb2c5736c184cf46d04ab7ccc491fac9879151
s-a-b-r <po_fasty_grisha@mail.ru>
2023-11-03 22:53:20 +0400

    Донастроил prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,7 +1,9 @@
 module.exports = {
     trailingComma: 'es5',
     tabWidth: 4,
-    semi: true,
+    semi: false,
     singleQuote: false,
+    bracketSameLine: true,
+    arrowParens: 'always',
     plugins: ['prettier-plugin-tailwindcss'],
 };
